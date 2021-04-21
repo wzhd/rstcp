isgasho/ustcp

Open TcpSocket to external users.

ustcp/src/iface/ethernet.rs
@@ -268,7 +268,7 @@ impl<'b, 'c, 'e, DeviceT> InterfaceBuilder<'b, 'c, 'e, DeviceT>
 }
 
 #[derive(Debug, PartialEq)]
-enum Packet<'a> {
+pub enum Packet<'a> {
     None,
     #[cfg(feature = "proto-ipv4")]
     Arp(ArpRepr),

wzhd/rstcp

src/iface/ethernet.rs
@@ -258,7 +258,7 @@ impl<'a, DeviceT> InterfaceBuilder<'a, DeviceT>
 }
 
 #[derive(Debug, PartialEq)]
-enum EthernetPacket<'a> {
+pub enum EthernetPacket<'a> {
     #[cfg(feature = "proto-ipv4")]
     Arp(ArpRepr),
     Ip(IpPacket<'a>),
@@ -266,7 +266,7 @@ enum EthernetPacket<'a> {
 
 
 #[derive(Debug, PartialEq)]
-pub(crate) enum IpPacket<'a> {
+pub enum IpPacket<'a> {
     #[cfg(feature = "proto-ipv4")]
     Icmpv4((Ipv4Repr, Icmpv4Repr<'a>)),
     #[cfg(feature = "proto-igmp")]